28ecc552668b78da5a7d74a9a8953899537b1b18
Dmitri Tsyganok <dmitri@tsyganok.net>
2018-12-10 10:49:34 +0200

    Code cleanup

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 'use strict';
 
 require('dotenv').config();
-const db      = require('./modules/db');
-const express = require('express');
-const app     = express();
-const fs      = require('fs');
-const multer  = require('multer');
-const upload  = multer({dest: 'pub/files/'});
-const id3     = require('node-id3');
-const mp3dur  = require('get-mp3-duration');
+const db    = require('./modules/db'),
+    express = require('express'),
+    app     = express(),
+    port    = process.env.PORT || 8000,
+    fs      = require('fs'),
+    multer  = require('multer'),
+    upload  = multer({dest: 'pub/files/'}),
+    id3     = require('node-id3'),
+    mp3dur  = require('get-mp3-duration');
 
 app.set('view engine', 'ejs');
 app.use(express.static('pub'));
@@ -62,7 +63,7 @@ app.use('/upload', (req, res) => {
   res.send('All done');
 });
 
-//listen to port 8000
-app.listen(8000, () => {
-  console.log("Listening to http://localhost:8000");
+//listen to port "PORT" or 8000
+app.listen(port, () => {
+  console.log("Listening to http://localhost:"+port);
 });
