57f0ffd22d95deb94cd65a80e518e75900debbde
Alena <tikalena@yandex.ru>
2018-12-11 10:34:39 +0200

    frontend login

--- pub/js/app.js
+++ pub/js/app.js
@@ -1,5 +1,18 @@
 'use strict';
 
-function validateForm() {
+document.getElementById('id01').addEventListener('submit', id01);
 
+function id01(event){
+  event.preventDefault();
+
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+
+  fetch('http://localhost:8000/login', {
+    method: 'POST',
+    headers : new Headers(),
+    body:JSON.stringify({username:username, password:password})
+  }).then((res) => res.json())
+.then((data) =>  console.log(data))
+.catch((err)=>console.log(err))
 }
